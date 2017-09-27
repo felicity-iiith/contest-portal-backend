470ceee8029cf6468c8d937a9760167fb57af9ef
sayak119 <sk.sayakkundu1997@gmail.com>
2017-09-28 04:00:48 +0530

    Added default values in User and Question models

--- src/models/Question.js
+++ src/models/Question.js
@@ -3,18 +3,37 @@ import db from './db'
 
 const Question = db.define('question', {
   qno: {
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      isInt: true,
+    }
   },
   // Add question type (other than string answer?)
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+    }
   },
   // Add provision for image
   body: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
   },
   answer: {
-    type: Sequelize.STRING
+    type: Sequelize.JSON,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    }
   },
 });
 
