99c8f22ae8193e1103cbb3a192f0c34ec34954c4
krutam <krutam.hathi@research.iiit.ac.in>
2017-09-28 03:05:41 +0530

    indented properly

--- src/middleware/isUnlocked.js
+++ src/middleware/isUnlocked.js
@@ -1,11 +1,12 @@
 export async function isUnlocked(ctx, next) {
+	const { user } = ctx.state
+	const { qno } = ctx.params
+	if(qno<=user.maxUnlock){
+		return next()
+	}
+	else ctx.response.status = 403
   // XXX: Check if the question has been unlocked by the user
   // Use ctx.state.user and ctx.params.qno
   // Do next only if it is unlocked
-  const { user } = ctx.state
-  const { qno } = ctx.params
-  if(qno<=user.maxUnlock){
-  	return next()
-  }
-  else ctx.body="Not Unlocked"
+
 }
