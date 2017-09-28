725bee3c80cf67d2e3df4a55466a29f40183a8ec
Kanav Gupta <kanavgupta0711@gmail.com>
2017-09-29 00:19:44 +0530

    Add /api/userinfo route (Fix #22) (#26)

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,5 +1,7 @@
 import koaRouter from 'koa-joi-router';
 
+import { isAuthenticated } from '../middleware/auth'
+
 const router = koaRouter();
 router.prefix('/api')
 
@@ -10,11 +12,20 @@ async function ctrl(ctx) {
     ctx.body = `Hello World!`
 }
 
+export async function userinfo(ctx) {
+  ctx.body = ctx.state.user
+}
+
 const routes = [
   {
     method: 'get',
     path: '/',
     handler: ctrl
+  },
+  {
+    method: 'get',
+    path: '/userinfo',
+    handler: [isAuthenticated, userinfo]	
   },
 ];
 
