48aaac41d5e7e1b430ca5c9e13fdc5a2bed8d5bb
Sayak Kundu <sk.sayakkundu1997@gmail.com>
2017-09-28 06:38:23 +0000

    Added default values in User and Question models (#13)

--- src/models/User.js
+++ src/models/User.js
@@ -3,16 +3,29 @@ import db from './db'
 
 const User = db.define('user', {
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.TEXT,
+    unique: true,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
   },
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
   },
   score:{
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0
   },
   maxUnlock:{
-    type: Sequelize.INTEGER
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 1
   },
 });
 
